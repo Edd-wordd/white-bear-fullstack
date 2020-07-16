62e0d9b18584f675051e90184f3b0e00f4a0c308
Edd-wordd <edward.plasencio@gmail.com>
2020-07-16 11:52:09 -0700

    prepare for deploy

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 app.use("/api/v1/users", require("./api/v1/users"));
 app.use("/api/v1/memory-cards", require("./api/v1/memory-cards"));
 
+app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+   res.sendFile(path.resolve(_dirname, "client", "build", "index.html"));
+});
 const port = process.env.PORT || 3045;
 app.listen(port, () =>
    console.log(`server running at https://localHost:${port}`)
